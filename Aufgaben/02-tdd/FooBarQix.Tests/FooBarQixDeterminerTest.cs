d5466494dc275329e960257a6f67e2a0f07fd882
Tschawi <89629023+Tschawi@users.noreply.github.com>
2022-11-02 15:34:09 +0100

    Schule aufgaben

--- Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
+++ Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
@@ -5,8 +5,31 @@ namespace FooBarQix.Tests
     public class FooBarQixDeterminerTest
     {
         [Fact]
-        public void Test1()
+        public void Determin_returnNumber()
         {
+            var number = 4;
+            var testee = new FooBarQixDeterminer();
+            var result = testee.Determine(number);
+
+            Assert.Equal(number.ToString(), result);
+        }
+        [Fact]
+        public void Determin_returnFoo()
+        {
+            var number = 3;
+            var testee = new FooBarQixDeterminer();
+            var result = testee.Determine(number);
+
+            Assert.Equal("Foo", result);
+        }
+        [Fact]
+        public void Determin_returnBar()
+        {
+            var number = 5;
+            var testee = new FooBarQixDeterminer();
+            var result = testee.Determine(number);
+
+            Assert.Equal("Bar", result);
         }
     }
 }
--- Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
+++ Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
@@ -1,7 +1,21 @@
-﻿namespace FooBarQix
+﻿using System;
+
+namespace FooBarQix
 {
     public class FooBarQixDeterminer
     {
+        public string Determine(int number)
+        {
+            if (number % 3 == 0)
+            {
+                return "Foo";
+            }
+            if (number % 5 == 0)
+            {
+                return "Bar";
+            }
+            return number.ToString();
+        }
 
     }
 }
